9315d025e396d2206e859000b055635c1f040b2c
alliecatstanz34 <alliecatstanz34@gmail.com>
2020-09-09 09:05:13 -0700

    updated example

--- ClassExample.java
+++ ClassExample.java
@@ -1,34 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
+//import java.awt.*; 
 
-public class ClassExample
-{
-    public static void tempChange(double value, int flag) {
-    	if(flag == 0){
-        	double newTempC = 5 * (value - 32) / 9;
-        	System.out.println("Farenheit: " + value + "\n Celcius: " + newTempC);
-        }
-        else{
-            double newTempF = (value * 9 / 5) +32;
-            System.out.println("Celcius: " + value + "\n Farenheit: " + newTempF);
-        }
-    }
-    
-    public static void main(String[] args){
-    
-        Scanner readme = new Scanner(System.in);
-        
-        System.out.println("Enter Two Numbers... first one in Farenheit and the second in Celsius");
-        System.out.println("Press Enter after each input: ");
-        
-        double faren, celsius;
-        
-        faren = readme.nextDouble();
-        celsius = readme.nextDouble();
-        
-        tempChange(faren, 0);
-        tempChange(celsius, 1);
+public class ClassExample { 
 
-        readme.close();
- 
-    }		
-}
+	public static void main(String args[]) 
+	{ 
+		// Creating object of the 
+		// class linked list 
+		LinkedList<String> ll 
+			= new LinkedList<String>(); 
+
+		// Adding elements to the linked list 
+		ll.add("A"); 
+		ll.add("B"); 
+		ll.addLast("C"); 
+		ll.addFirst("D"); 
+		ll.add(2, "E"); 
+
+		System.out.println(ll); 
+
+		ll.remove("B"); 
+		System.out.println(ll); 
+
+		ll.remove(3); 
+	
+		System.out.println(ll); 
+
+		ll.removeFirst(); 
+		ll.removeLast(); 
+
+		System.out.println(ll); 
+	} 
+} 
