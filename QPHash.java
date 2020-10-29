042fe1cc9a57154cc37a017a39a4f15164f57bdb
alliecatstanz34 <alliecatstanz34@gmail.com>
2020-10-29 10:18:14 -0700

    wednesday week 10

--- QPHash.java
+++ QPHash.java
@@ -1,58 +1,70 @@
 
 
-
 class QPHash{
 
-    static void printArray(int arr[]){
-        for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
-        }
-    }
-
-    static void hashing(int htable[], int tsize, int arr[], int size){
-
-        //iterate through array 
-        for(int i = 0; i < size; i++){
-
-            //computing the hash value 
-            int hvalue = arr[i] % tsize;
-
-            //insert into the table if no collisions 
-            if(htable[hvalue] == -1){
-                htable[hvalue] = arr[i];
-            } else { // quadratic probing 
-                for (int j = 0; j < tsize; j++){
-                    int newHvalue = (hvalue +(j*j)) % tsize;
-
-                    if(htable[newHvalue] == -1){
-                        htable[newHvalue] = arr[i];
-                        break;
-                    }
-                }
-            }
+	static void printArray(int arr[])
+	{
+		for(int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+	}
 
 
-        }
+	static void hashing(int htable[], int tsize, int arr[], int size)
+	{
+		//iterate through the array
 
-        printArray(htable);
+		for (int i = 0; i < size; i++)
+		{
+			//computinh the hash value here
+			int hvalue = arr[i] % tsize;
 
-    }
 
-    public static void main(String args []){
+			// insert into the table if no collisions
 
-        int array[] = {50,700,76,85,92,73,101};
-        int n = 7;
-        int l = 11;
-        
-        int hashTable[] = new int [l];
+			if(htable[hvalue] == -1)
+				htable[hvalue] = arr[i];
+			else // quadratic probing 
+			{
+				for(int j = 0; j < tsize; j++)
+				{
+					int newHvalue = (hvalue + (j*j)) %tsize;
 
-        for(int i = 0; i < l; i++){
-            hashTable[i] = -1;
-        }
+					if(htable[newHvalue] == -1)
+					{
+						htable[newHvalue] = arr[i];
+						break;
+					}
+				}
+					
+			}
+		}
 
-        printArray(array);
+		printArray(htable);
+	}
 
-        hashing(hashTable, l, array, n);
-    }
+	public static void main(String args[])
+	{
+		int array[] = {50, 700, 76, 85, 92, 73, 101};
+
+		int n = 7;
+
+		int l = 11;
+
+
+		int hashTable[] = new int [l];
+
+		for(int i = 0; i < l; i++)
+		{
+			hashTable[i] = -1;
+		}
+
+		printArray(array);
+
+		hashing(hashTable, l, array, n);
+	}
+
+
+
+
 
 }
